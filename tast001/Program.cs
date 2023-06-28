75e8043a4771b6cf3ec69d05ca3342e0c33752c6
Anastasia <didyoumissme.19s@gmail.com>
2023-06-28 21:56:26 +0300

    tast001 and tast 002

---
+++ tast001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+void Main()
+{
+    int[] array = FillArray(5, 100, 999);
+    PrintArray(array);
+    int sum = 0;
+    Res(array, out sum);
+    System.Console.WriteLine(sum);
+}
+int[] FillArray(int size = 5, int left = 100, int right = 999)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(left, right);
+    }
+    return array;
+}
+void Res(int[] array, out int sum)
+{
+    sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        { sum = sum + 1; }
+    }
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+Main();
---
+++ tast002/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+void main()
+{
+    int[] array = FillArray(5, -999, 999);
+    PrintArray(array);
+    int sum = 0;
+    Res(array, out sum);
+    System.Console.WriteLine(sum);
+}
+int[] FillArray(int size = 5, int left = -999, int right = 999)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(left, right);
+    }
+    return array;
+}
+void Res(int[] arr, out int sum)
+{
+    sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        { sum = sum + arr[i]; }
+    }
+
+}
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+main();
